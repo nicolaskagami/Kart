41004d725d633b2c6a38e3b33af1e8e266dff0d3
nicolassk <nicolaskagami@gmail.com>
2015-11-15 17:56:09 -0200

    Refactorization + shell model + ground and sky

--- src/objects/item.js
+++ src/objects/item.js
@@ -1,9 +1,9 @@
 var Shell = function(newPosition,newDirection)
 {
-	this.position = newPosition;
+	this.position = newPosition.slice();
 	this.speed = 0.1; 
 	this.angle = 0;
-	this.direction = newDirection;
+	this.direction = newDirection.slice();;
 	//this.collisionArea = new CollidableCircle(x,z,0.8); // fix this
 	this.model = models.greenShell;
 	this.scale = [0.05,0.05,0.05];
